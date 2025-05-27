48bf8f2b55df981c8a324fcd1500869f88feccdc
NaP <154236950+Danadanarealna@users.noreply.github.com>
2025-05-27 09:48:17 +0700

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,2 @@
 const String apiBaseUrl = 'http://localhost:8000/api';
 const bool isWeb = bool.fromEnvironment('dart.library.html');
-
-// const String apiBaseUrl = isWeb
-//     ? 'http://18.139.98.108/api'
-//     : 'http://192.168.0.108:8000/api'; 
